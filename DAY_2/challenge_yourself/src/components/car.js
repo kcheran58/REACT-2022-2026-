ea23e4c3a11d942adf18cf4d508f7d72d962df32
kcheran58 <119770229+kcheran58@users.noreply.github.com>
2023-11-16 10:14:47 +0530

    Add files via upload

---
+++ DAY_2/challenge_yourself/src/components/car.js
@@ -0,0 +1,16 @@
+import React from 'react';
+class Car extends React.Component
+{
+    render() {
+        return (
+          <>
+            <h1>Car : Rolls Royce</h1>
+            <img
+              src="https://images.dealer.com/ddc/vehicles/2022/Rolls-Royce/Phantom/Sedan/color/Iguazu%20Blue-R93-16,24,64-640-en_US.jpg"
+              alt="Rolls-Royce"
+            />
+          </>
+        )
+    }
+}
+export default Car;
---
+++ class_tasks/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import List from './components/listDemo';
+
+function App() {
+  return (
+    <>
+      <List />
+    </>
+  );
+}
+
+export default App;
